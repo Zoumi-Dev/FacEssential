209f9ff39a97614d56f55a0f06e6263dbb83640c
Zoumi-Dev <venosia.pro@gmail.com>
2021-03-14 15:32:09 +0100

    test

--- src/Zoumi/FacEssential/listener/PlayerListener.php
+++ src/Zoumi/FacEssential/listener/PlayerListener.php
@@ -8,16 +8,19 @@ use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use Zoumi\FacEssential\api\Home;
 use Zoumi\FacEssential\api\Money;
+use Zoumi\FacEssential\api\Scoreboard;
 use Zoumi\FacEssential\Main;
 use Zoumi\FacEssential\Manager;
+use Zoumi\FacEssential\task\ScoreboardTask;
 
 class PlayerListener implements Listener {
 
     public function onPreLogin(PlayerPreLoginEvent $event){
         $player = $event->getPlayer();
         if (Main::getInstance()->manager->get("enable-money")){
-            if (!Money::haveAccount($player)) {
+            if (!Money::haveAccount($player->getName())) {
                 Money::createAccount($player);
             }
         }
@@ -29,6 +32,18 @@ class PlayerListener implements Listener {
         if (!$player->hasPlayedBefore()){
             FunctionListener::sendBroadcastByFormat("message", str_replace("{player}", $player->getName(), Main::getInstance()->manager->get("connect-disconnect")["first-connect-message"]));
         }
+        if (Main::getInstance()->manager->get("scoreboard")["enable"]){
+            $scoreboard = Main::getInstance()->scoreboard[$player->getName()] = new Scoreboard($player);
+            $config = Main::getInstance()->manager;
+            $line_actus = 0;
+            foreach ($config->get("scoreboard")["lines"] as $line){
+                $scoreboard
+                    ->setLine($line_actus, str_replace(["{money}"], [Money::getMoney($player->getName())], $line))->set();
+                $line_actus++;
+            }
+            Main::getInstance()->getScheduler()->scheduleRepeatingTask(new ScoreboardTask($player), $config->get("scoreboard")["update-tick"]);
+        }
+        Home::addHome($player, "test");
         FunctionListener::sendBroadcastByFormat(Main::getInstance()->manager->get("connect-disconnect")["format"], str_replace("{player}", $player->getName(), Main::getInstance()->manager->get("connect-disconnect")["connect-message"]));
     }
 
